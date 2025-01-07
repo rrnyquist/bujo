8f84624455345f3a9f46b5e53e4fdf3ed0a6a3b6
rnyquist <richard.nyquist.ctr@au.af.edu>
2025-01-06 19:16:58 -0500

    Turned into a proper plugin with bullet cycling

--- README.md
+++ README.md
@@ -1,36 +1,64 @@
-# The BuJo CSS Snippet for Obsidian
+# The Bullet Journal Rapid Logging Plugin for Obsidian
 I recently came across the [Bullet Journal Method](https://bulletjournal.com/). Something that impressed me about this method was how simple the basic tenets of the "rapid logging" system were: just a few types of bullets, each one quick to handwrite and easy to read. All the non-actionable bullet items take up the space of a block letter, while the actionable ones (the bullets) are pretty easy to pick out. Instantly, I wanted to implement the format into my favorite note-taking app, [Obsidian](https://obsidian.md/). Although some of the same bullet formatting tecniques already exist in a wonderful Obsidian theme called [Minimal](https://minimal.guide/home/) (I highly recommend you go check it out), I decided the formatting syntax needed to be _just so_; and so I decided to [make something from scratch](https://x.com/vincentdnl/status/1268573228626333703). 
 
 If you like the bullet journal method, you are now able to get that "BuJo" look with just a few lines of CSS code from this repo. It has been tested on both the default and Minimal themes. 
 
-Enjoy!
+As is common to such projects, much of the code in this repo was born from helpful resources on the internet. The primary two were [Hotkeys++](https://publish.obsidian.md/hub/02+-+Community+Expansions/02.05+All+Community+Expansions/Plugins/hotkeysplus-obsidian) and a [post in an Obsidian forum](https://forum.obsidian.md/t/custom-bullet-points/83764/2) by hPiper. Be sure to check them out!
+
+I hope you enjoy this plugin!
 
 # Getting Started
-Place bujo.css in your `.obsidian > snippets` directory, and enable it from Obsidian's `Settings > Appearance` menu.
-For more information on adding CSS snippets, see [this guide](https://help.obsidian.md/Extending+Obsidian/CSS+snippets).
+Clone this folder to your `.obsidian > plugins` directory, and enable it from Obsidian's `Options > Community Plugins` menu.
+For more information on adding custom plugins, see [this guide](https://docs.obsidian.md/Plugins/Getting+started/Build+a+plugin).
 
-The BuJo syntax is a modified version of the default task list (checkboxes). In _Reader_ mode, only the "todo" (`- [ ]`) and "done" (`- [x]`) checkboxes may be toggled, while all other symbols remain locked. In _Live Editor_ mode, any bullet can be toggled to enter a "todo" (`- [ ]`) state. In _Source_ mode, you can view the raw markdown (such as in the example below).
+The Rapid Logging syntax is the same as your typical checkboxes. That is, 
 
-I recommend adding links to the "migrated" (`- [>]`) and scheduled (`- [<]`) line items. This is why their default color is the same as unpopulated Obsidian links. Even if you choose not to link to other files, the lighter color emphasizes that these tasks still remain for completion at another time.
+```
+- [ ] todo
+- [x] done
+```
+or
+```
+* [ ] todo
+* [x] done
+```
+
+Rapid Logging just adds more task variations. Namely:
+
+```
+- [ ] todo
+- [x] done
+- [o] event
+- [-] note
+- [>] migrated
+- [<] scheduled
+- [~] canceled
+```
+
+In Obsidian, all the above tasks (except for "todo") will be represented with a completed checkmark. What this plugin does is enable custom formatting for each type, and allow the user to cycle through the plugin types in the order listed above (todo, done, event, note...) using a hotkey. By default, this hotkey is set to `Ctrl + M`. You can change this hotkey by searching "Rapid Logging" in `Options > Hotkeys`.
 
 ### Syntax Example
 ```
-* [ ] todo
-* [x] done
-* [o] event
-	* [-] note
-	* [-] note
-	* [-] note
-* [ ] todo
-* [ ] todo
-* [x] done
-* [~] canceled
-* [>] migrated
-* [ ] todo
-* [ ] todo
-* [<] scheduled
+- [ ] todo
+- [x] done
+- [o] event
+	- [-] note
+	- [-] note
+	- [-] note
+- [ ] todo
+- [ ] todo
+- [x] done
+- [~] canceled
+- [>] migrated
+- [ ] todo
+- [ ] todo
+- [<] scheduled
 ```
-### Example Results
-![BuJo HTML Result](https://github.com/rrnyquist/bujo/blob/main/readme_example.png)
+### Rendered Results
+![BuJo HTML Result](./extras/readme_example.png)
+
+In reader mode, only the "todo" (`* [ ]`) and "done" (`* [x]`) checkboxes can be toggled by clicking, while the other symbols remain locked. 
+
+I recommend adding links to the "migrated" (`* [>]`) and scheduled (`* [<]`) line items (this is why their default highlighting is the same color as unpopulated links in Obsidian--it also clues you in to the tasks that you'll have to do another time).
 
 If you don't like the icons I have here, it shouldn't be too hard to change them yourself! Have fun--assuming, for some sick reason, that's the kind of thing you do for fun. And if it _is_... uh, wanna get lunch later? You like sushi?
---
+++ main.js
@@ -0,0 +1,178 @@
+'use strict';
+
+var obsidian = require('obsidian');
+
+/* The base structure of this file was originally Hotkeys++, created by Argentina Ortega Sainz. It was modified by Richard Nyquist for the purpose of Rapid Logging bullet cycling.*/
+/* https://argentinaos.com */
+
+
+var extendStatics = function(d, b) {
+    extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+    return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+    if (typeof b !== "function" && b !== null)
+        throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+var RapidLogging = /** @class */ (function (_super) {
+    __extends(RapidLogging, _super);
+    function RapidLogging() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    RapidLogging.prototype.onload = function () {
+        var _this = this;
+        console.log('Loading RapidLogging plugin');
+        
+        this.addCommand({
+            id: 'toggle-task',
+            name: 'Toggle task lists',
+            callback: function () { return _this.toggleTodos(); },
+            hotkeys: [
+                {
+                    modifiers: ['Mod'],
+                    key: 'm',
+                },
+            ],
+        });
+
+    
+    };
+    RapidLogging.prototype.cleanSelected = function () {
+        var view = this.app.workspace.getActiveViewOfType(obsidian.MarkdownView);
+        if (!view)
+            return;
+        var editor = view.editor;
+        var selectedText = this.getSelectedText(editor);
+        var newString = selectedText.content.trim().replace(/(\r\n|\n|\r)/gm, ' ');
+        newString = newString.replace(/  +/gm, ' ');
+        editor.replaceRange(newString, selectedText.start, selectedText.end);
+    };
+    RapidLogging.prototype.onunload = function () {
+        console.log('Unloading RapidLogging plugin');
+    };
+    RapidLogging.prototype.getSelectedText = function (editor) {
+        if (editor.somethingSelected()) {
+            // Toggle to-dos under the selection
+            var cursorStart = editor.getCursor('from');
+            var cursorEnd = editor.getCursor('to');
+            var content = editor.getRange({ line: cursorStart.line, ch: 0 }, { line: cursorEnd.line, ch: editor.getLine(cursorEnd.line).length });
+            return {
+                start: { line: cursorStart.line, ch: 0 },
+                end: {
+                    line: cursorEnd.line,
+                    ch: editor.getLine(cursorEnd.line).length,
+                },
+                content: content,
+            };
+        }
+        else {
+            // Toggle the todo in the line
+            var lineNr = editor.getCursor().line;
+            var contents = editor.getDoc().getLine(lineNr);
+            var cursorStart = {
+                line: lineNr,
+                ch: 0,
+            };
+            var cursorEnd = {
+                line: lineNr,
+                ch: contents.length,
+            };
+            var content = editor.getRange(cursorStart, cursorEnd);
+            return { start: cursorStart, end: cursorEnd, content: content };
+        }
+    };
+
+    RapidLogging.prototype.toggleElement = function (re, subst) {
+        var view = this.app.workspace.getActiveViewOfType(obsidian.MarkdownView);
+        if (!view)
+            return;
+        var editor = view.editor;
+        var selection = editor.somethingSelected();
+        var selectedText = this.getSelectedText(editor);
+        var newString = selectedText.content.replace(re, subst);
+        // console.log(newString);
+        editor.replaceRange(newString, selectedText.start, selectedText.end);
+        // Keep cursor in the same place
+        if (selection) {
+            editor.setSelection(selectedText.start, {
+                line: selectedText.end.line,
+                ch: editor.getLine(selectedText.end.line).length,
+            });
+        }
+    };
+    RapidLogging.prototype.toggleTodos = function () {
+        // * [ ] todo
+        // * [x] done
+        // * [o] event
+        // * [-] note
+        // * [>] migrated
+        // * [<] scheduled
+        // * [~] canceled
+        // Defaults to dashes, but keeps in whatever bullet style you originally chose.
+        var re = /(^\s*|^\t*)(\*\s\[\s\]\s|\*\s\[x\]\s|\*\s\[o\]\s|\*\s\[\-\]\s|\*\s\[>\]\s|\*\s\[<\]\s|\*\s\[~\]\s|-\s\[\s\]\s|-\s\[x\]\s|-\s\[o\]\s|-\s\[\-\]\s|-\s\[>\]\s|-\s\[<\]\s|-\s\[~\]\s|\*\s|-\s|\d*\.\s|\*\s|\b|)([^\n\r]*)/gim;
+        return this.toggleElement(re, this.replaceTodoElement);
+    };
+
+    RapidLogging.prototype.replaceTodoElement = function (match, spaces, startText, sentence) {
+        // console.log(startText);
+        if (startText === '* [ ] ') {
+            return spaces + '* [x] ' + sentence;
+        }
+        else if (startText === '* [x] ') {
+            return spaces + '* [o] ' + sentence;
+        }
+        else if (startText === '* [o] ') {
+            return spaces + '* [-] ' + sentence;
+        }
+        else if (startText === '* [-] ') {
+            return spaces + '* [>] ' + sentence;
+        }
+        else if (startText === '* [>] ') {
+            return spaces + '* [<] ' + sentence;
+        }
+        else if (startText === '* [<] ') {
+            return spaces + '* [~] ' + sentence;
+        }
+        else if (startText === '* [~] ') {
+            return spaces + '* [ ] ' + sentence;
+        }
+        else if (startText === '- [ ] ') {
+            return spaces + '- [x] ' + sentence;
+        }
+        else if (startText === '- [x] ') {
+            return spaces + '- [o] ' + sentence;
+        }
+        else if (startText === '- [o] ') {
+            return spaces + '- [-] ' + sentence;
+        }
+        else if (startText === '- [-] ') {
+            return spaces + '- [>] ' + sentence;
+        }
+        else if (startText === '- [>] ') {
+            return spaces + '- [<] ' + sentence;
+        }
+        else if (startText === '- [<] ') {
+            return spaces + '- [~] ' + sentence;
+        }
+        else if (startText === '- [~] ') {
+            return spaces + '- [ ] ' + sentence;
+        }
+        else {
+            return spaces + '- [ ] ' + sentence;
+        }
+
+    };
+
+    return RapidLogging;
+}(obsidian.Plugin));
+
+module.exports = RapidLogging;
+  
+/* nosourcemap */
